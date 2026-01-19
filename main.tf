f14bd82c4d9a777704a2a9b742287b6597352277
jerin-hc <jerin.johnykutty@hashicorp.com>
2026-01-19 09:26:30 +0530

    updated iam

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 terraform {
-cloud { 
-  organization = "integrations-sdk" 
-  workspaces { 
-    name = "aws-ec2" 
-  } 
-}
+  cloud {
+    organization = "integrations-sdk"
+
+    workspaces {
+      name = "aws-ec2"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -17,11 +19,28 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (update for your region)
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.small"
 
   tags = {
     Name = "terraform-ec2-instance"
   }
 }
+
